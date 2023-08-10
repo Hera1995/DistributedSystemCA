cf8105720b866662e1da890557b2247d95cc7266
Hera <heraleung1995@gmail.com>
2023-08-10 16:01:02 +0100

    update pom file

--- src/main/java/SmartBuilding/Main.java
+++ src/main/java/SmartBuilding/Main.java
@@ -1,4 +1,4 @@
-package shanliang;
+package SmartBuilding;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
